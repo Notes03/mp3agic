0a4bf8e18a2d0bf69ee5ffe358778f19ed678aeb
Manuel <manuel.scholz@outlook.com>
2017-09-30 09:08:54 +0200

    Add xxx to lyrics to make it international

--- src/main/java/com/mpatric/mp3agic/AbstractID3v2Tag.java
+++ src/main/java/com/mpatric/mp3agic/AbstractID3v2Tag.java
@@ -744,7 +744,7 @@ public abstract class AbstractID3v2Tag implements ID3v2 {
 	public void setLyrics(String lyrics) {
 		if (lyrics != null && lyrics.length() > 0) {
 			invalidateDataLength();
-			ID3v2CommentFrameData frameData = new ID3v2CommentFrameData(useFrameUnsynchronisation(), "eng", null, new EncodedText(lyrics));
+			ID3v2CommentFrameData frameData = new ID3v2CommentFrameData(useFrameUnsynchronisation(), "xxx", null, new EncodedText(lyrics));
 			addFrame(createFrame(ID_TEXT_LYRICS, frameData.toBytes()), true);
 		}
 	}
